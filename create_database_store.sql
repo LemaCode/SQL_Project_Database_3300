8ed880bbf2c59bb9a9dcefc172a2d8aa10606605
Manasse Bosango <manassebosango@gmail.com>
2024-02-19 23:57:07 -0800

    Look

--- create_database_store.sql
+++ create_database_store.sql
@@ -73,7 +73,7 @@ CREATE TABLE Car
     Maker_ID       INT,
     Color          VARCHAR(50),
     Model_ID       INT,
-    Condition_Type VARCHAR(50),
+    Condition_Type VARCHAR(100),
     Car_Picture    VARCHAR(255),
     Price          DECIMAL(10, 3),
     FOREIGN KEY (Category_ID) REFERENCES Category (Category_ID),

6b55203dfd933cb334257e3180d420fb675f47bf
Manasse Bosango <111810878+LemaCode@users.noreply.github.com>
2024-03-11 11:13:11 -0700

    Add files via upload

---
+++ Milestone 4/query5.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "cssql.seattleu.edu";
+$username = "bd_atidjani";
+$password = "8AN9QY2G";
+$dbname = "bd_atidjani";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// // Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT Customer.Customer_ID, Customer.First_Name, Customer.Last_Name, COUNT(Orders.Order_ID) AS TotalOrders
+FROM Customer
+LEFT JOIN Orders ON Customer.Customer_ID = Orders.Customer_ID
+GROUP BY Customer.Customer_ID, Customer.First_Name, Customer.Last_Name;";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+echo "<table border = '1'>\n";
+// output data of each row
+while($row = mysqli_fetch_row($result)) {
+echo "<tr>\n";
+$cols = mysqli_num_fields($result);
+for ($i = 0; $i < $cols; $i++) {
+echo "<td>" . $row[$i] . "</td>\n";
+}
+echo "</tr>\n";
+}
+echo "</table>\n";
+} else {
+echo "0 results";
+}
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+?>
+
